76d6f33c66a0c3416ca4d323ff71537753f8624b
AloynixSix-0910 <Lgm_0710v@outlook.com>
2025-09-22 15:39:02 -0500

    Add files via upload

--- Ejercicio 7 triqui.cpp
+++ Ejercicio 7 triqui.cpp
@@ -15,14 +15,13 @@ int main(){
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     };
-    char jugador_actual = 'X';
+    char jugador = 'X';
     int fila, columna;
-    bool ganado = false;
+    bool win = false;
 
     //El bucle principal se repite 9 veces (el máximo de turnos).
     for (int turno = 0; turno < 9; turno++) {
 
-        // Dibujar el tablero en cada turno que pasa
         cout << "   0   1   2" << endl;
 
         cout << "0 " << tablero[0][0] << "  |  " << tablero[0][1] << "|  " << tablero[0][2] << endl;
@@ -36,12 +35,7 @@ int main(){
         cout << "2 " << tablero[2][0] << "  |  " << tablero[2][1] << "|  " << tablero[2][2] << endl;
 
 
-
-
-
-        //Pide la jugada
-
-        cout << "Turno del Jugador\n '" << jugador_actual << "'. Ingresa fila y columna (ej 1 presione enter y luego 2 y presione enter): ";
+        cout << "Turno del Jugador\n '" << jugador << "'. Ingresa fila y columna (ej 1 presione enter y luego 2 y presione enter): ";
         cin >> fila >> columna;
 
 
@@ -52,42 +46,40 @@ int main(){
             columna >= 0 &&
             columna < 3 &&
             tablero[fila][columna] == ' ') {
-            tablero[fila][columna] = jugador_actual;
+            tablero[fila][columna] = jugador;
 
-            // 5. Reviso si alguien ganó
 
             // Revisa filas
-            if ((tablero[fila][0] == jugador_actual &&
-            tablero[fila][1] == jugador_actual &&
-            tablero[fila][2] == jugador_actual) ||
+            if ((tablero[fila][0] == jugador &&
+            tablero[fila][1] == jugador &&
+            tablero[fila][2] == jugador) ||
 
             // Reviso columnas
-                (tablero[0][columna] == jugador_actual &&
-                tablero[1][columna] == jugador_actual &&
+                (tablero[0][columna] == jugador &&
+                tablero[1][columna] == jugador &&
 
-                tablero[2][columna] == jugador_actual) ||
+                tablero[2][columna] == jugador) ||
 
-            // Reviso diagonal n1
-                (tablero[0][0] == jugador_actual &&
-                tablero[1][1] == jugador_actual &&
-                tablero[2][2] == jugador_actual) ||
+            // Reviso diagonal 1
+                (tablero[0][0] == jugador &&
+                tablero[1][1] == jugador &&
+                tablero[2][2] == jugador) ||
 
             // Revisa diagonal 2
-                (tablero[2][0] == jugador_actual &&
-                tablero[1][1] == jugador_actual &&
-                tablero[0][2] == jugador_actual))
+                (tablero[2][0] == jugador &&
+                tablero[1][1] == jugador &&
+                tablero[0][2] == jugador))
             {
-                ganado = true;
-                break; // Si alguien gana,se rompe el bucle
+                win = true;
+                break; //
             }
 
 
-            // 6. Cambio el turno al otro jugador(aqui me perdi)
-            if (jugador_actual == 'X') {
-                jugador_actual = 'O';
+            if (jugador == 'X') {
+                jugador= 'O';
 
             } else {
-                jugador_actual = 'X';
+                jugador = 'X';
             }
 
         } else {
@@ -98,10 +90,10 @@ int main(){
 
     // 7.resultado final
     cout << "\n---FIN DEL JUEGO ---" << endl;
-    if (ganado) {
-        cout << "¡El jugador '" << jugador_actual << "' ha ganado!" << endl;
+    if (win) {
+        cout << "¡El jugador '" << jugador<< "' ha ganado!" << endl;
     } else {
-        cout << "¡Es un empate!" << endl; //me cae mal ese jugador
+        cout << "¡Es un empate!" << endl;
     }
 
     return 0;
